fc686e36cf370bce86fce5f0300b43f7cb43682c
Tyler Kiesman <tylerkiesman@gmail.com>
2020-05-04 18:32:23 -0400

    Created function that finds average yearly temp and uncertainty, updated to larger fonts

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.generatePoints import *
 
 curDir = os.path.dirname(__file__)
 global_temp_path = os.path.abspath(os.path.join(curDir, "..", "data", "GlobalTemperatures.csv"))
-global_yearly_points = generate_global_yearly(global_temp_path)
+l_and_o_yearly_data = land_and_ocean_yearly(global_temp_path)
 
 app = Flask(__name__)
 
--- app/generatePoints.py
+++ app/generatePoints.py
@@ -1,4 +1,26 @@
-def generate_global_yearly(filepath):
+def add_to_dict(key, value, dict):
+    if key in dict:
+        dict[key] = dict.get(key) + value
+    else:
+        dict[key] = value
+
+def land_and_ocean_yearly(filepath):
     file = open(filepath, 'r')
-    firstline = file.readline()
+    year_to_temps = dict()
+    year_to_uncert = dict()
+
+    #Get rid of first line that only is the column names
+    file.readline()
     data = file.readlines()
+    for line in data:
+        split_data = line.split(',')
+        year = int(split_data[0].split('-')[0])
+        if year >= 1850:
+            monthly_temp = float(split_data[split_data.__len__() - 2])
+            monthly_uncert = float(split_data[split_data.__len__() - 1])
+            add_to_dict(year, monthly_temp, year_to_temps)
+            add_to_dict(year, monthly_uncert, year_to_uncert)
+    for key in year_to_temps:
+        year_to_temps[key] = year_to_temps.get(key) / 12
+        year_to_uncert[key] = year_to_uncert.get(key) / 12
+    return [year_to_temps, year_to_uncert]
