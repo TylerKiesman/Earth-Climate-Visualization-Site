a1d73a16e05103d9fcbd69438883db33ca87685d
Tyler Kiesman <tylerkiesman@gmail.com>
2020-05-10 19:02:31 -0400

    Updated to have buttons to swap between the two graphs using AJAX calls

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
-from app import yearly_fig, prediction_val, period1_fig, period2_fig
+from app import yearly_fig, prediction_val, period1_fig, period2_fig, year1, year2, years_after
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', plot=yearly_fig, pred=prediction_val, period1=period1_fig, period2=period2_fig)
+    return render_template('index.html', plot=yearly_fig,
+                           pred=prediction_val, period=period1_fig, year1=year1, year2=year2,
+                           ped1=year1 + years_after, ped2=year2 + years_after)
+
+@app.route('/updatePeriod')
+def update_period():
+    number = request.args.get('number')
+    number = int(number)
+    if number == 1:
+        return period1_fig
+    elif number == 2:
+        return period2_fig
--- app/static/js/loadPlots.js
+++ app/static/js/loadPlots.js
@@ -1,5 +1,25 @@
 function showPlot(graphs, id){
     data = graphs[0].data;
     layout = graphs[0].layout;
-    Plotly.plot(id, data, layout);
+    Plotly.newPlot(id, data, layout);
+}
+
+function updatePeriod(periodNumber){
+    var request = new XMLHttpRequest();
+
+    // Instantiating the request object
+    request.open("GET", "updatePeriod?number=" + periodNumber);
+
+    // Defining event listener for readystatechange event
+    request.onreadystatechange = function() {
+        // Check if the request is compete and was successful
+        if(this.readyState === 4 && this.status === 200) {
+            // Inserting the response from server into an HTML element
+            console.log(JSON.parse(request.response)[0])
+            showPlot(JSON.parse(request.response), 'period');
+        }
+    };
+
+    // Sending the request to the server
+    request.send();
 }
